4cc7a372a20678e8065c3d9db1eca2854c718d8d
DRD_23 <164327433+DRD-23@users.noreply.github.com>
2024-03-26 15:36:06 +0100

    f

--- admin.py
+++ admin.py
@@ -1,9 +1,21 @@
-import class
-import class2
+from athlete import Athlete
+from visiteur import Visiteur
+from pays import Pays
 from mysql.connector import connect
-
-class ActeurDAO:
+liste0=[]
+class Administrateur:
     def connection():
         bdd=connect(host="localhost",user="root",
                     password="root",database="Olympiques")
         return(bdd)
+
+def lire_athlete(self):
+        bdd=self.connection()
+        cursor=bdd.cursor()
+        cursor.execute("SELECT ath_nom, ath_prenom, ath_naissance, act_pays, act_discipline, act_recompense FROM Athletes")
+        resultat=cursor.fetchall()
+        for iathlete in resultat:
+            nom, prenom, naissance, pays, discipline, recompense= iathlete
+            athlete0=Athlete(nom,prenom,naissance,pays,discipline, recompense)
+            liste0.append(athlete0)
+        return(liste0)
