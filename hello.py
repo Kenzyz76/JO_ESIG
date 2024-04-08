991fedd2989e79c8faa31ea31518eb5001753278
Kenzyz76 <92575631+Kenzyz76@users.noreply.github.com>
2024-04-08 17:32:47 +0200

    Add files via upload
    Couleur bouton + appel d'une fonction

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 
 import tkinter as tk
+from tkinter import font
+import admin
 
 def effacer():
     cadre_bouton_athlete.destroy()
@@ -9,9 +11,9 @@ def afficher():
     text = tk.Label(text = "")
     text.place(x=200, y=200)
 
-def creer_bouton(fenetre, texte, commande, largeur, hauteur):
-    bouton = tk.Button(fenetre, text=texte, command=lambda: commande(), width=largeur, height=hauteur)
-    bouton.pack()
+def creer_bouton(fenetre, texte, commande, largeur, hauteur, couleur,police, taille):
+    bouton = tk.Button(fenetre, text=texte, command=lambda: commande(), width=largeur, height=hauteur, bg=couleur, font=(police, taille))
+    bouton.pack() #permet d'afficher le bouton
     return bouton
 
 def afficher_interface_athlete():
@@ -25,7 +27,9 @@ def afficher_interface_athlete():
     cadre_gauche.pack(side="left", fill="y")
 
     # Ajouter trois boutons au Frame de gauche
-    creer_bouton(cadre_gauche, f"Afficher tous les athlètes", afficher, 15, 2)
+    creer_bouton(cadre_gauche, f"Afficher tous les athlètes", admin.admin.afficher_athlete, 20, 2, '#9ef0f6','Tw Cen MT',12)
+    creer_bouton(cadre_gauche, f"Afficher par pays", afficher, 20, 2,'#6ecaf2','Tw Cen MT',12)
+    creer_bouton(cadre_gauche, f"Afficher par discipline", afficher, 20, 2, '#2969eb','Tw Cen MT',12)
 
     # Créer un Frame central pour le champ de texte et le menu déroulant
     cadre_central = tk.Frame(fenêtre)
@@ -39,7 +43,7 @@ def afficher_interface_athlete():
     options_menu = ["Option 1", "Option 2", "Option 3"]
     variable_menu = tk.StringVar(fenêtre)
     variable_menu.set(options_menu[0])  # Définir l'option par défaut
-    menu_deroulant = tk.OptionMenu(cadre_central, variable_menu, options_menu)
+    menu_deroulant = tk.OptionMenu(cadre_central, variable_menu, *options_menu)
     menu_deroulant.pack()
 
 
@@ -48,8 +52,11 @@ def afficher_interface_athlete():
     cadre_droit.pack(side="right", fill="y")
 
     # Ajouter trois boutons au Frame de droite
-    for i in range(1, 4):
-        creer_bouton(cadre_droit, f"Bouton D{i}", afficher, 3, 3)
+    creer_bouton(cadre_droit, f"caca", afficher, 20, 2,'#9ef0f6','Tw Cen MT',12)
+    creer_bouton(cadre_droit, f"pipi", afficher, 20, 2,'#6ecaf2','Tw Cen MT',12)
+    creer_bouton(cadre_droit, f"popo", afficher, 20, 2,'#2969eb','Tw Cen MT',12)
+
+
 
 def afficher_interface_visiteur():
     effacer()
@@ -63,7 +70,7 @@ def afficher_interface_visiteur():
 
     # Ajouter trois boutons au Frame de gauche
     for i in range(1, 4):
-        creer_bouton(cadre_gauche, f"Bouton G{i}", afficher)
+        creer_bouton(cadre_gauche, f"Bouton G{i}", afficher, 20,2, 'red','Tw Cen MT',12)
 
     # Créer un Frame central pour le champ de texte et le menu déroulant
     cadre_central = tk.Frame(fenêtre)
@@ -86,23 +93,27 @@ def afficher_interface_visiteur():
 
     # Ajouter trois boutons au Frame de droite
     for i in range(1, 4):
-        creer_bouton(frame, f"Bouton D{i}", afficher)
+        creer_bouton(frame, f"Bouton D{i}", afficher,20,2,'Tw Cen MT',12)
 
 # Créer la fenêtre principale
 fenêtre = tk.Tk()
 fenêtre.title("Interface Dynamique avec Animations de Bouton")
+fenêtre.configure(bg='#f4fefe')
 
 cadre_bouton_athlete = tk.Frame(fenêtre)
 cadre_bouton_athlete.pack(side="left", fill="x", expand=True)
+cadre_bouton_athlete.configure(bg ='#f4fefe')
+
 
 cadre_bouton_visiteur = tk.Frame(fenêtre)
 cadre_bouton_visiteur.pack(side="right", fill="x", expand=True)
+cadre_bouton_visiteur.configure(bg ='#f4fefe')
 # Définir la taille de la fenêtre à 900x500 pixels
 fenêtre.geometry("900x500")
 fenêtre.minsize(500,300)
 # Créer et placer deux boutons initiaux au centre
-bouton1 = creer_bouton(cadre_bouton_athlete, "Athlètes", afficher_interface_athlete, 20, 20)
-bouton2 = creer_bouton(cadre_bouton_visiteur, "Visiteurs", afficher_interface_visiteur, 20, 20)
+bouton1 = creer_bouton(cadre_bouton_athlete, "Athlètes", afficher_interface_athlete, 20, 10, '#5FD4EE','Tw Cen MT',20)
+bouton2 = creer_bouton(cadre_bouton_visiteur, "Visiteurs", afficher_interface_visiteur, 20, 10, '#EE5F5F','Tw Cen MT',20)
 
 # Démarrer la boucle événementielle de l'interface graphique
 fenêtre.mainloop()
