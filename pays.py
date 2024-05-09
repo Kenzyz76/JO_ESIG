f7a0b3f7444b60fef040c857ad37a0fc0f96832c
DRD_23 <164327433+DRD-23@users.noreply.github.com>
2024-05-09 11:54:53 +0200

    Delete pays.py
    Ne sert plus à rien

--- pays.py
+++
@@ -1,7 +0,0 @@
-class Pays:
-    def __init__(self,nom0,continent0):
-        self.nom=nom0
-        self.continent=continent0
-
-    def afficher (self):
-        return ([self.nom,self.continent])
